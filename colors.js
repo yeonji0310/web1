fcccd8ad54ea99d1d4968825154bdc0987a669a3
yeonji0310 <63365970+yeonji0310@users.noreply.github.com>
2020-05-22 22:24:09 +0900

    JS 8-10
    4주차 강의 과제 업로드

---
+++ colors.js
@@ -0,0 +1,41 @@
+
+var Links= {
+    setColor: function(color) { 
+//   var alist = document.querySelectorAll('a');
+//   var i=0;
+//   while (i < alist.length){
+//   alist[i].style.color = 'powderblue';
+//   i=i+1;}
+//    
+    $('a').css('color', color);//웹페이지에 있는 모든 a 태그를 제이쿼리로 제어하겠다는 뜻
+    }
+ }
+
+  var Body= {
+    setColor:function(color){
+    //   document.querySelector('body').style.color = color;},
+   $('body').css('color',color);}
+    setBackgroundColor:function(color){
+    //   document.querySelector('body').style.backgroundColor = color;}
+   $('body').css('backgroundColor',color);
+ }
+  
+  function nightDayHandler(self) {
+    
+  var target = document.querySelector('body');
+  if(self.value === 'night')
+  {Body.setBackgroundColor=('black');
+   Body.setColor=('white');
+   self.value = 'day';
+   
+   Links.setColor('powderblue'); 
+   }
+  
+  else{
+   Body.setBackgroundColor=('white');
+   Body.setColor=('black');
+   self.value= 'night';
+  
+   Links.setColor('blue');
+     }
+    } 
